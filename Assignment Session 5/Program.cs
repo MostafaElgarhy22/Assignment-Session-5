9da1f199d433c70595012dfbab1755df90a648df
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-13 03:42:07 +0200

    Third Project

--- Assignment Session 5/Program.cs
+++ Assignment Session 5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Assignment_Session_5.First_Project;
 using Assignment_Session_5.Second_Project;
+using Assignment_Session_5.Third_Project;
 
 namespace Assignment_Session_5
 {
@@ -33,18 +34,66 @@ namespace Assignment_Session_5
             #endregion
             #region Second Project
 
-            int Addition = Maths.Add(20, 10);
-            Console.WriteLine($"Addition = {Addition}");
+            //int Addition = Maths.Add(20, 10);
+            //Console.WriteLine($"Addition = {Addition}");
 
-            int Subtraction = Maths.Subtract(20, 10);
-            Console.WriteLine($"Subtraction = {Subtraction}");
+            //int Subtraction = Maths.Subtract(20, 10);
+            //Console.WriteLine($"Subtraction = {Subtraction}");
 
-            int Multiplication = Maths.Multiply(20, 10);
-            Console.WriteLine($"Multiplication = {Multiplication}");
+            //int Multiplication = Maths.Multiply(20, 10);
+            //Console.WriteLine($"Multiplication = {Multiplication}");
 
-            double Division = Maths.Divide(20, 10);
-            Console.WriteLine($"Division = {Division}");
+            //double Division = Maths.Divide(20, 10);
+            //Console.WriteLine($"Division = {Division}");
             #endregion
+            #region Third Project
+
+            Console.WriteLine("Enter UserType of : (Regular, Premium , Guest)");
+            string userType = Console.ReadLine();
+
+            User user;
+            if(userType == "Regular")
+            {
+                user = new RegularUser("Regular User");
+            }
+            else if (userType == "Premium")
+            {
+                user = new RegularUser("Premium User");
+            }
+            else if (userType == "Guest")
+            {
+                user = new RegularUser("Guest User");
+            }
+            else
+            {
+                user = null;
+            }
+
+
+
+
+            if (user == null)
+            {
+                Console.WriteLine("Invalid user type.");
+                return;
+            }
+
+            Console.WriteLine("Enter product price:");
+             decimal.TryParse(Console.ReadLine(), out decimal price);
+
+            Console.WriteLine("Enter product quantity:");
+            int.TryParse(Console.ReadLine(), out int quantity);
+
+            Discount discount = user.GetDiscount();
+            decimal discountAmount = discount?.CalculateDiscount(price, quantity) ?? 0; // لو مفيش Discount هيبقا ب 0
+            decimal finalPrice = (price * quantity) - discountAmount;
+
+            Console.WriteLine($"User: {user.Name}");
+            Console.WriteLine($"Discount Type: {discount?.Name ?? "No Discount"}");
+            Console.WriteLine($"Discount Amount: {discountAmount:C}");
+            Console.WriteLine($"Final Price: {finalPrice}");
         }
+        #endregion
     }
+    
 }
---
+++ Assignment Session 5/Third Project/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Session_5.Third_Project
+{
+    public abstract class User
+    {
+        public string Name { get; set; }
+        public abstract Discount GetDiscount();
+    }
+    public class RegularUser : User
+    {
+        public RegularUser(string name)
+        {
+            Name = name;
+        }
+
+        public override Discount GetDiscount()
+        {
+            return new PercentageDiscount(5);
+        }
+    }
+
+    public class PremiumUser : User
+    {
+        public PremiumUser(string name)
+        {
+            Name = name;
+        }
+
+        public override Discount GetDiscount()
+        {
+            return new FlatDiscount(100);
+        }
+    }
+
+    public class GuestUser : User
+    {
+        public GuestUser(string name)
+        {
+            Name = name;
+        }
+
+        public override Discount GetDiscount()
+        {
+            return null; 
+        }
+    }
+}
