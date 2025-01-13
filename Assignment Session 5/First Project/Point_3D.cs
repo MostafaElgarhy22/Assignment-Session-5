dd1b7626a1ce537c28a5b2bab2f6072d622d2c07
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-13 02:26:18 +0200

    Add project files.

---
+++ Assignment Session 5/First Project/Point_3D.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Session_5.First_Project
+{
+    internal class Point_3D : IComparable<Point_3D>, ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+        public Point_3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Point_3D()
+        {
+            this.Z = 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Points is {X}, {Y}, {Z}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Point_3D)
+            {
+                return X == ((Point_3D)obj).X && Y == ((Point_3D)obj).Y && Z == ((Point_3D)obj).Z ;
+            }
+            return false ;
+        }
+ 
+        public int CompareTo(Point_3D other)
+        {
+            if (X != other.X) return X.CompareTo(other.X);
+            if (Y != other.Y) return X.CompareTo(other.Y);
+            return Z.CompareTo(other.Z);
+        }
+
+        public object Clone()
+        {
+            return new Point_3D(X, Y, Z);
+        }
+    }
+
+
+}
---
+++ Assignment Session 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using Assignment_Session_5.First_Project;
+
+namespace Assignment_Session_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region First Project
+
+
+            //Point_3D point_3D = new Point_3D(15, 4, 6);
+            //Console.WriteLine($"{point_3D.ToString()}");
+
+            //Point_3D PC = (Point_3D)PC.Clone();
+            //Console.WriteLine(PC);
+
+            //Point_3D[] P = new Point_3D[]
+            //{
+            //   new Point_3D(5, 6,10),
+            //   new Point_3D(10,32,7),
+            //   new Point_3D(1,1,1),
+            //   new Point_3D(1,8,9)
+            //};
+
+            //Array.Sort(P);
+            //foreach (var point in P)
+            //{
+            //    Console.WriteLine(point.ToString());
+            //}
+
+            #endregion
+        }
+    }
+}
